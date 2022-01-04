b4d69e35cb97e4290fa8d76dc22e6e6a9b4884ce
Mike Piech <mpiech@alum.mit.edu>
2022-01-03 22:38:06 -0800

    added mongo call

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from waitress import serve
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://atlas-db-user-1641158466980233578:NvS08*=y@cluster0.brwd1.mongodb.net/mystrj?retryWrites=true&w=majority")
+db = client.mystrk
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Dincas!"
+    return db.tracks.find_one()
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
